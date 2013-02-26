c8b3d82b0248f527f744da8ef15afd639c4b7d13
andrew.m.knight <andrew.m.knight@accenture.com>
2013-02-26 15:24:32 -0500

    adding icon for real, first version of new TF changes

--- backup-rally.py
+++ backup-rally.py
@@ -10,21 +10,23 @@ rally.enableLogging('mypyral.log')
 env = Environment(loader = FileSystemLoader('.'))
 template = env.get_template('testCase.html')
 
-if args[0] is not None:
- queryCriteria = 'Iteration.Name contains "{0}"'.format(args[0])
+if not args:
+ queryCriteria = 'Project.Name contains "{0}"'.format(project)
+else:
+ queryCriteria = 'Project.Name contains "{0}"'.format(args[0])
 
 # First we query once to get each Test Set for the given sprint
-response = rally.get('TestSet', fetch="TestCases,FormattedID", query=queryCriteria)
+response = rally.get('TestFolder', fetch="TestCases,FormattedID,Name", query=queryCriteria)
 if response.errors:
- sys.stdout.write("\n".join(errors))
+ sys.stdout.write("\n".join(response.errors))
  sys.exit(1)
 # Now we loop over the test sets, and then the test cases
-for testSet in response:
- thisSet = testSet.FormattedID
- testCases = rally.get('TestCase', fetch="FormattedID,PreConditions,Name,Steps",query="FormattedID = {0}".format(thisSet))
+for testFolder in response:
+ thisFolder = testFolder.FormattedID
+ testCases = rally.get('TestCase', fetch="FormattedID,PreConditions,Name,Steps,ValidationInput,Type,PostConditions",query="FormattedID = {0}".format(thisFolder))
  for testCase in testCases:
-  fileName = "{0}-{1}.html".format(thisSet, testCase.FormattedID)
-  savedTC = open(fileName, 'w')
-  savedTC.write(template.render(tc_name=testCase.Name, steps=testCase.Steps, preconditions=testCase.PreConditions).encode('utf-8', 'ignore'))
+  fileName = "{0}_{1}-{2}_{3}.html".format(thisFolder, testFolder.Name, testCase.FormattedID, testCase.Name)
+  savedTC = open(fileName[:256], 'w') #truncate filename to 256 characters
+  savedTC.write(template.render(tc_name=testCase.Name, steps=testCase.Steps, preconditions=testCase.PreConditions, validationInput=testCase.ValidationInput).encode('utf-8', 'ignore'))
   savedTC.close()
   print "{0} saved.".format(fileName)
